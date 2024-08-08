606720d2668a61b1c2fe733937ead0a5992ea67b
SebasYyaXD <131383823+SebasYyaXD@users.noreply.github.com>
2024-08-08 07:24:07 -0600

    Add files via upload

---
+++ admin/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from .forms import ContactoForm
+
+
+def contacto(request):
+    if request.method == 'POST':
+        form = ContactoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contacto')
+    else:
+        form = ContactoForm()
+    return render(request, 'myapp/index.html', {'form': form})
